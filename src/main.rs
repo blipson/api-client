3aaf409578d6173d9d8192167b56b0ebfa438e5c
Ben.Lipson <ben.lipson@target.com>
2025-08-18 21:28:03 -0500

    run request

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
-use iced::Element;
-use iced::widget::{Column, button, column, row, text_input};
+use iced::widget::{Column, button, column, row, text_input, Space, Container, Text, scrollable};
+use iced::{Element, Font, Length, Theme};
+use serde_json::Value;
 
 impl ApiClient {
     fn view(&self) -> Column<Message> {
@@ -18,11 +19,43 @@ impl ApiClient {
                 .on_input(Message::Type)
                 .on_submit(Message::SubmitRequest)
                 .padding(5)
+                .width(Length::FillPortion(1))
                 .into()
         });
 
-        column![row![button("Create").on_press(Message::ToggleDropdown)].push_maybe(request_box)]
-            .push_maybe(dropdown)
+        let run_request: Option<Element<Message>> = self.show_request_box.then(|| {
+            button("Run")
+                .on_press(Message::RunRequest)
+                .into()
+        });
+
+        let request_space: Option<Space> = (self.show_request_box && !self.show_response).then(|| {
+            Space::with_width(Length::FillPortion(1))
+        });
+
+        let response: Option<Element<Message>> = self.show_response.then(|| {
+            Container::new(
+                scrollable(
+                    Text::new(&self.response)
+                        .font(Font::MONOSPACE)
+                        .size(14)
+                )
+                    .height(Length::Fixed(240.0))
+                    .width(Length::Fill)
+            )
+                .padding(10)
+                .width(Length::Fill)
+                .into()
+        });
+
+        column![
+            row![button("Create").on_press(Message::ToggleDropdown)]
+                .push_maybe(request_box)
+                .push_maybe(run_request)
+                .push_maybe(request_space)
+                .push_maybe(response)
+        ]
+        .push_maybe(dropdown)
     }
 }
 
@@ -31,6 +64,8 @@ struct ApiClient {
     show_dropdown: bool,
     show_request_box: bool,
     text_value: String,
+    show_response: bool,
+    response: String,
 }
 
 #[derive(Debug, Clone)]
@@ -39,27 +74,35 @@ enum Message {
     ToggleDropdown,
     CreateRequest,
     CreateFolder,
+    RunRequest,
     SubmitRequest,
 }
 
 impl ApiClient {
     fn update(&mut self, message: Message) {
         match message {
-            Message::Type(value) => {
-                self.text_value = value
-            }
+            Message::Type(value) => self.text_value = value,
             Message::ToggleDropdown => {
                 self.show_dropdown = !self.show_dropdown;
             }
             Message::CreateRequest => {
-                println!("Create Request triggered");
                 self.show_dropdown = false;
                 self.show_request_box = true;
             }
             Message::CreateFolder => {
-                println!("Create Folder triggered");
                 self.show_dropdown = false;
             }
+            Message::RunRequest => match reqwest::blocking::get(&self.text_value) {
+                Ok(response) => {
+                    self.show_response = true;
+                    let json: Value = response.json().expect("");
+                    self.response = json.to_string();
+                }
+                Err(error) => {
+                    self.show_response = true;
+                    self.response = "Error".to_string();
+                }
+            },
             Message::SubmitRequest => {
                 println!("{}", "submitting...")
             }
